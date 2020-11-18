b74218168cc35155501821b5d112b9da78a31cbc
Stéphane Severio <severio.stephane@gmail.com>
2020-11-18 13:19:53 +0100

    Seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+create_resto1 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: (Restaurant::CATEGORIES.sample))
+create_resto1.save!
+puts " Restos done !"
+end
+# create_resto2 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: 4)
+# create_resto3 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: 5)
+# create_resto4 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: 0)
+# create_resto5 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: 1)
