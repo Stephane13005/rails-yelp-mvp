c82623f9d17c2ab7e47483c0a081e61e08cdfdea
Stéphane Severio <severio.stephane@gmail.com>
2020-11-18 16:04:06 +0100

    Exercise done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
+  RATINGS = [0, 1, 2, 3, 4, 5].freeze
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true }
-  validates_inclusion_of :rating, in: [0, 1, 2, 3, 4, 5]
+  validates :rating, inclusion: { in: 0..5, message: ' should be between 0 to 5' }
+  validates :rating, presence: { message: ' cannot be blank' }, numericality: { only_integer: true }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
+# frozen_string_literal: true
 
 5.times do
-create_resto1 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: (Restaurant::CATEGORIES.sample))
-create_resto1.save!
-puts " Restos done !"
+  create_resto1 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: Restaurant::CATEGORIES.sample)
+  create_resto1.save!
+  puts ' Restos done !'
 end
-# create_resto2 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: 4)
-# create_resto3 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: 5)
-# create_resto4 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: 0)
-# create_resto5 = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.phone_number, category: 1)
